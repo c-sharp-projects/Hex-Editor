08c221954b5a1588919ee58aa8f2b1cf62453e77
harshalghule1896 <harshalghule07@gmail.com>
2019-02-26 22:25:01 +0530

    Hex Editor commit 1

---
+++ HexaEditor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HexEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+  
+
+    public partial class MainWindow : Window
+    {
+       // static public String FileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Listener(object sender, RoutedEventArgs e)
+        {
+
+            String FileName = TextBox.Text;
+
+            if (FileName.Equals(""))
+            {
+                MessageBox.Show("Please Enter EXE Name To Explore !");
+                return;
+            }
+            else if (!(File.Exists(FileName)))
+            {
+                MessageBox.Show("File Is Not Present In Current Directory !");
+                return;
+            }
+
+            Window2 obj = new Window2(FileName);
+            
+            obj.Show();
+            this.Close();
+        }
+
+    }
+}
---
+++ HexaEditor/Window2.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HexEditor
+{
+    
+    public partial class Window2 : Window
+    {
+        public String FileName;
+        String HexString;
+        String AsciiString;
+
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        public Window2(String FileName) : this()
+        {
+           this.FileName = FileName;
+            Loadfile();
+        }
+
+        public void Loadfile()
+        {
+         
+            FileName = Path.GetFullPath(FileName);
+
+            
+            byte[] buffer = File.ReadAllBytes(@"" + FileName);
+           
+                AsciiString = Encoding.ASCII.GetString(buffer);
+                String[] hex = BitConverter.ToString(buffer).Split('-');
+
+                foreach (String s in hex)
+                {
+                    HexString += s;
+                    HexString += "  ";
+                }
+
+                HexText.Text = HexString;
+
+                AsciiText.Text = AsciiString;
+              
+
+        }
+
+        private void Listener(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            int index = 0;
+
+            switch (btn.Uid)
+            {
+                case "FindString":
+                    if ((index = AsciiString.ToLower().IndexOf(StringFind.Text.ToLower())) == -1)
+                    {
+                        Status1.Content = "Status : String Not Found";
+                    }
+                    else
+                    {
+                        Status1.Content = "The Position Of Element " + StringFind.Text + " is " + index;
+                    }
+
+                    break;
+
+                case "FindHex":
+
+                    if ((index = HexString.ToLower().IndexOf(HexFind.Text.ToLower())) == -1)
+                    {
+                        Status2.Content = "Status : String Not Found";
+                    }
+                    else
+                    {
+                        Status2.Content = "The Position Of Element " + HexFind.Text + " is " + index;
+                    }
+                    break;
+            }
+
+        }
+    }
+}
